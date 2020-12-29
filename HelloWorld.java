319fc8b5854f58598e5bf4e344e2f0b0bf95da5a
sachinrkodiya <sachinrkodiya@users.noreply.github.com>
2020-12-29 19:52:27 +0530

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+// Prints "Hello, World" to the terminal window.
+		System.out.println("Hello, World");
+		System.out.println("Vaishnavi");
+		System.out.println("Sachin");
+	}
+
+}
