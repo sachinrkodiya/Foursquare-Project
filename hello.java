95e0d90f28f98cd29d249f62c6e55fbcb46e0162
sachinrkodiya <sachinrkodiya@users.noreply.github.com>
2020-12-29 18:49:26 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,12 +1,18 @@
-package hellovalue;
+
+package Sample;
 
 public class hello {
-	public String sayHello() {
-		return "Hello";
+	public static void main(String[] args) {  
+	    //defining a variable  
+	    int number=13;
+	    if(number==0) {
+		System.out.println("zero");
+	    }  
+	    //Check if the number is divisible by 2 or not  
+	    if(number%2==0){  
+	        System.out.println("even number");  
+	    }else{  
+	        System.out.println("odd number");  
+	    }  
 	}
-	public static void main(String[] args) {
-	hello obj = new hello();
-	System.out.println(obj.sayHello());	
-	}
-
 }
