6ee52330e2f81b51750433b9045cd265d778fb96
Vaishnavi-RBhat <vaishnavi.ar@robosoftin.com>
2020-12-29 19:00:08 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,6 +4,9 @@ public class hello {
 	public static void main(String[] args) {  
 	    //defining a variable  
 	    int number=13;  
+		if(number==0){
+			System.out.println("VaISHNAVI");
+		}
 	    //Check if the number is divisible by 2 or not  
 	    if(number%2==0){  
 	        System.out.println("even number");  
