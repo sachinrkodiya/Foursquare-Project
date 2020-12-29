42e870592e1d4dbae665a33e1c0770594edeb8a7
sachinrkodiya <sachinrkodiya@users.noreply.github.com>
2020-12-29 19:05:16 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,18 @@
+
+package Sample;
+
+public class hello {
+	public static void main(String[] args) {  
+	    //defining a variable  
+	    int number=13;
+	    if(number==0) {
+		System.out.println("zero");
+	    }  
+	    //Check if the number is divisible by 2 or not  
+	    if(number%2==0){  
+	        System.out.println("even number");  
+	    }else{  
+	        System.out.println("odd number");  
+	    }  
+	}
+}
